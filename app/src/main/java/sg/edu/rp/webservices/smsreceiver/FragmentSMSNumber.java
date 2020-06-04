810d0141ee72565b91e39f67d3aa9adf63d3647e
18045945 <aungmoethet2000@gmail.com>
2020-06-04 21:18:25 +0800

    Fragments XML -> settled

---
+++ app/src/main/java/sg/edu/rp/webservices/smsreceiver/FragmentSMSNumber.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.webservices.smsreceiver;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSMSNumber extends Fragment {
+
+    public FragmentSMSNumber() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_s_m_s_number, container, false);
+    }
+}
